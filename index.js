ed52c210ace08f1659d14d6fdf909b6bdfabce59
rhyyyyzs <officialrevir02@gmail.com>
2026-04-06 22:34:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+function goToAdobo() {
+  window.location.href = "Recipeforadobo.html";
+}
+
+function Return() {
+  window.location.href = "..group2recipes.html";
+}
+
+function goToBicolExpress() {
+  window.location.href = "bicolexpressrecipe.html";
+}
+
+function goTokarekare() {
+  window.location.href = "karekarerecipe.html";
+}
+
+function goChickenTinola() {
+  window.location.href = "tinolarecipe.html";
+}
+
+function goToPinakbet() {
+  window.location.href = "pinakbet.html";
+}
+
+function goToGinataangBangus() {
+  window.location.href = "ginataangbangus.html";
+}
+
+function goToSarciadongIsda() {
+  window.location.href = "sarciadongisda.html";
+}
+
+function goToBulalo() {
+  window.location.href = "bulalo.html";
+}
+
+function goToGarlicPepperBeef() {
+  window.location.href = "garlicpepperbeef.html";
+}
+
+function goToPaksiw() {
+  window.location.href = "paksiw.html";
+}
+
+function goToChickenAfritada() {
+  window.location.href = "chickenafritada.html";
+}
+
+function goToMenudo() {
+  window.location.href = "menudo.html";
+}
+
+function goToSisig() {
+  window.location.href = "sisig.html";
+}
+
